c20016765c3edb7ce64bf6304fdf5ed3a0d24107
Joe Krump <joe.krump@clio.com>
2019-04-11 10:47:38 -0700

    Drawing circles with margin

--- sketch.js
+++ sketch.js
@@ -1,23 +1,43 @@
 const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math');
 
 const settings = {
-  dimensions: "A4", // could be "A4" or "postcard" or something like that.
-  units: "cm",
-  pixelsPerInch: 300,
+  dimensions: [2048, 2048]
 };
 
 const sketch = () => {
+  const createGrid =  () => {
+    const points = [];
+    const count = 5;
+
+    for(let x = 0; x < count; x++) {
+      for(let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push([u, v]);
+      }
+    }
+
+    return points;
+  };
+
+  const points = createGrid();
+  const margin = 400;
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'blue';
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    context.beginPath();
-    context.arc(width / 2, height / 2 , width * 0.2, 0, Math.PI * 2, false);
-    context.fillStyle = "red";
-    context.fill();
-    context.lineWidth = width * 0.05;
-    context.strokeStyle = "black";
-    context.stroke();
+    points.forEach(([u, v]) => {
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
+
+      context.beginPath();
+      context.arc(x, y, 100, 0, Math.PI * 2, false);
+      context.strokeStyle = "black";
+      context.lineWidth = 3;
+      context.stroke();
+    });
   };
 };
 
