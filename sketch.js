e8e196096bf3afbae5a9d78fecf12b48f5478118
Joe Krump <joe.krump@clio.com>
2019-04-11 11:14:55 -0700

    Using 2D noise to generate radius values

--- sketch.js
+++ sketch.js
@@ -19,9 +19,11 @@ const sketch = () => {
       for(let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v)) * 0.05;
+
         points.push({
           color: random.pick(palette),
-          radius: Math.max(0, random.gaussian()) * 0.03,
+          radius,
           position: [u, v]
         });
       }
@@ -30,7 +32,7 @@ const sketch = () => {
     return points;
   };
 
-  random.setSeed(12);
+  // random.setSeed(12);
 
   const points = createGrid().filter(() => {
     return (random.value() > 0.5);
